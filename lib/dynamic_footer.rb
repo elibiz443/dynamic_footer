0163cc6b3e614c1f0c121932ea87bc4ddf215a54
Elly Ambet <ellyambet10@gmail.com>
2023-07-13 15:57:12 +0300

    Initial Commit

---
+++ lib/dynamic_footer.rb
@@ -0,0 +1,8 @@
+# frozen_string_literal: true
+
+require_relative "dynamic_footer/version"
+
+module DynamicFooter
+  class Error < StandardError; end
+  # Your code goes here...
+end
---
+++ lib/dynamic_footer/version.rb
@@ -0,0 +1,5 @@
+# frozen_string_literal: true
+
+module DynamicFooter
+  VERSION = "0.1.0"
+end
